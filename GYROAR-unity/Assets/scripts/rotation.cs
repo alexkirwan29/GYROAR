19e8b05b60236116f66198bc80ba5d0aef381e0f
alexkirwan29 <alexkirwan29@gmail.com>
2018-04-28 13:38:44 +0800

    Added Unity Project to the repo.
    Initial commit! :+1:

---
+++ GYROAR-unity/Assets/scripts/rotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotation : MonoBehaviour {
+
+    public Slider xSlider;
+    public Slider ySlider;
+    public Slider zSlider;
+
+    //public Text xDegree;
+    //public Text yDegree;
+    //public Text zDegree;
+
+    public GameObject theObject;
+
+	// Use this for initialization
+	void Start () {
+
+        xSlider.maxValue = 360;
+        ySlider.maxValue = 360;
+        zSlider.maxValue = 360;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+      
+        	
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GYROAR
+## Bring your 3D creations into your stream; REALTIME!
+
+#### Under Development since 27.4.2018
